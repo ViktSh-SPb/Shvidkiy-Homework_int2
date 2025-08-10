25b3021363bb47aed1f1d58a959c86e579883f17
Viktor <vikt.sh@gmail.com>
2025-08-10 19:35:30 +0300

    Создал утилиту для Hibernate. Написал тестовый код для С-операции и создания таблицы в БД

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,13 +15,39 @@ public class App
     {
         User user = new User("Alice", "Alice@gmail.com", 20, LocalDateTime.now());
         saveUser(user);
-        System.out.println( "Hello World!" );
+
+        User loaded;
     }
 
     public static void saveUser(User user){
         try (Session session = HibernateUtil.getSessionFactory().openSession()){
             Transaction tx = session.beginTransaction();
             session.merge(user);
+            tx.commit();
+        }
+    }
+
+    public static User getUser(Long id){
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            return session.get(User.class, id);
+        }
+    }
+
+    public static void updateUser(User user){
+        try(Session session=HibernateUtil.getSessionFactory().openSession()){
+            Transaction tx = session.beginTransaction();
+            session.merge(user);
+            tx.commit();
+        }
+    }
+
+    public static void deleteUser(Long id){
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            Transaction tx = session.beginTransaction();
+            User user = session.get(User.class, id);
+            if(user!=null){
+                session.remove(user);
+            }
             tx.commit();
         }
     }
