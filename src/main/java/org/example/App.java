d20e6f0b46c16d74b721fb85d9fc0bc71d89d2d1
Viktor <vikt.sh@gmail.com>
2025-08-10 19:55:04 +0300

    Создал демонстрацию crud операций

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,50 +2,55 @@ package org.example;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-
 import java.time.LocalDateTime;
 
 /**
- * Hello world!
- *
+ * @author Viktor Shvidkiy
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         User user = new User("Alice", "Alice@gmail.com", 20, LocalDateTime.now());
         saveUser(user);
 
-        User loaded;
+        User loaded = getUser(user.getId());
+        System.out.println("Read: " + loaded);
+
+        loaded.setName("Sara");
+        updateUser(loaded);
+        System.out.println("Update: " + loaded);
+
+        deleteUser(loaded.getId());
+        System.out.println("Delete: "+ getUser(loaded.getId()));
+
     }
 
-    public static void saveUser(User user){
-        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+    public static void saveUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Transaction tx = session.beginTransaction();
-            session.merge(user);
+            session.persist(user);
             tx.commit();
         }
     }
 
-    public static User getUser(Long id){
-        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+    public static User getUser(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             return session.get(User.class, id);
         }
     }
 
-    public static void updateUser(User user){
-        try(Session session=HibernateUtil.getSessionFactory().openSession()){
+    public static void updateUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Transaction tx = session.beginTransaction();
             session.merge(user);
             tx.commit();
         }
     }
 
-    public static void deleteUser(Long id){
-        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+    public static void deleteUser(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Transaction tx = session.beginTransaction();
             User user = session.get(User.class, id);
-            if(user!=null){
+            if (user != null) {
                 session.remove(user);
             }
             tx.commit();
